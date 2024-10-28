bc23360e29ef58d5e4482c8356233f5f71fbd7be
Vincent Zhong <suppernoname@163.com>
2024-10-29 00:37:00 +0800

    change api

--- his-client/src/api/patientView/patientData.ts
+++ his-client/src/api/patientView/patientData.ts
@@ -2,29 +2,20 @@ import request from "@/utils/request2.ts";
 
 //获取病人数据
 export const getPatientsData = (params: any) => {
-  return request.get('/patient/query', { params: params })
+  return request.get('/Patient/findPatient', { params: params })
 }
 
 //患者删除请求
-// export const patientDeleteRequest = (params: any) => {
-//   return request.delete('/patient/delete',{params: params})
-// }
 export const patientDeleteRequest = (patientId: number) => {
-  return request.delete(`/patient/delete/${patientId}`)
+  return request.delete(`/Patient/deletePatient${patientId}`)
 }
 //患者编辑请求
 export const patientEditRequest = (params: any) => {
-  return request.put('/patient/edit', params )
+  return request.post('/Patient/revisePatient', params )
 }
 
 //患者新增请求
 export const patientAddRequest = (params: any) => {
   console.log('新增',params)
-  return request.post('/patient/add',params)
+  return request.post('/Patient/addPatient',params)
 }
-
-
-//获取病区数据
-// export const getWardsData = () => {
-//   return request.get('/patients')
-// }
