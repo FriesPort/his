63d359bfd1aa0db81ce36bb0a19246e8dd5165e1
luo <1390191013@qq.com>
2024-11-03 19:09:59 +0800

    修改患者管理部分功能

--- his-server/his-domain/src/main/java/com/example/dto/patient/PatientAlterDTO.java
+++ his-server/his-domain/src/main/java/com/example/dto/patient/PatientAlterDTO.java
@@ -31,7 +31,6 @@ public class PatientAlterDTO {
     private Integer isvip;                // 是否 VIP（0 否, 1 是）
     private Integer isacute;              // 是否重症
     private String illness;               // 疾病类型
-    private String createby;              // 创建人
 
 }
 
