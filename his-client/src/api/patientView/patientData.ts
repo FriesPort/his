9e0240d69184a32cdc54b9997f9dc32a29b33275
Vincent Zhong <suppernoname@163.com>
2024-11-04 21:43:03 +0800

    fix url and http method

--- his-client/src/api/patientView/patientData.ts
+++ his-client/src/api/patientView/patientData.ts
@@ -1,21 +1,22 @@
-import request from "@/utils/request2.ts";
+import request from "@/utils/request.ts";
 
-//获取病人数据
+//查找患者信息
 export const getPatientsData = (params: any) => {
-  return request.get('/Patient/findPatient', { params: params })
+  return request.post('/patient/query', params)
 }
 
 //患者删除请求
-export const patientDeleteRequest = (patientId: number) => {
-  return request.delete(`/Patient/deletePatient${patientId}`)
+export const patientDeleteRequest = (params: any) => {
+  return request.post(`/patient/delete`, params)
 }
+
 //患者编辑请求
 export const patientEditRequest = (params: any) => {
-  return request.post('/Patient/revisePatient', params )
+  return request.post('/patient/edit', params )
 }
 
 //患者新增请求
 export const patientAddRequest = (params: any) => {
   console.log('新增',params)
-  return request.post('/Patient/addPatient',params)
+  return request.post('/patient/adds',params)
 }
